ad6b048df308eed2f107a50fdf9db9c1e227c479
AjayBarot-BTC <ajay.barot@beaverlogic.com>
2014-06-27 19:07:46 +0530

    final

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ MyProject::Application.routes.draw do
   get "static_pages/ContactUs"
   get "static_pages/home"
   resources :projects
-
+  root to: "static_pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
